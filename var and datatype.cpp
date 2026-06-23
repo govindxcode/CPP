1c5d4b9a30a4abf9967c7d45125178b51b8e08e7
Govind <ranagovind6969@gmail.com>
2026-06-23 17:52:31 +0530

    added few concepts of variable

--- var and datatype.cpp
+++ var and datatype.cpp
@@ -1,4 +1,9 @@
-#include<iostream>
+ //                     UPDATION OR MANIPULATION 
+
+
+
+
+/*include<iostream>
 using namespace std ;
 
 int main (){
@@ -11,4 +16,79 @@ int main (){
 
     return 0 ;
 
+}*/
+
+
+//             DOUBLE DATA TYPE AND PRECISION 
+
+
+
+/*#include <iostream>
+using namespace std ;
+
+int main() {
+    double num = 55.6988447;
+    cout << fixed << setprecision(7) << num;
+} */
+
+
+
+//         SIZE OF DATA TYPE 
+
+/*#include <iostream>
+using namespace std ;
+
+int main(){
+
+    int age = 21; 
+    cout << sizeof(int) << endl ;
+    
+    return 0;
+}*/
+
+
+
+
+//        DOUBLE OR NESTED BLOCK UPDATION AND MANIPULATION OF SAME VARIABLE
+//         USING TWO DIFF BLOCKS 
+
+
+/*#include<iostream>
+using namespace std ;
+
+int main(){
+
+    int age =21 ;
+    cout << "age is "<< age << endl;
+
+    { 
+        int age =25 ;
+        cout << "now the age is " << age << endl ;
+    }
+
+    return 0;
+}*/
+
+
+//               OR (NESTED BLOCKS )
+
+
+#include<iostream>
+using namespace std ;
+
+int main(){
+
+    { int age =21 ;
+    cout << "age is "<< age << endl;
+
+    }
+        int age =25 ;
+        cout << "now the age is " << age << endl ;
+    
+
+    return 0;
 }
+
+
+
+ 
