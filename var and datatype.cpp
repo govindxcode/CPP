5ea68c12fac095e4721ff364f7f3477be11d9940
Govind <ranagovind6969@gmail.com>
2026-06-23 15:28:41 +0530

    added var and data type program

---
+++ var and datatype.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std ;
+
+int main (){
+
+    int age = 21;
+    cout << "The age was "<< age <<  endl;
+
+    age = 45 ;
+    cout << " Now the age is " << age << endl ; 
+
+    return 0 ;
+
+}
