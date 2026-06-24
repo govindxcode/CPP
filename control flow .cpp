b52e9f2e773e11aad9cc643caa7a7546dd63eb40
Govind <ranagovind6969@gmail.com>
2026-06-24 21:53:40 +0530

    added topic control flow if else

---
+++ control flow .cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int budget ;
+    cout << "enter your budget "<<endl;
+
+    cin >> budget;
+
+    if(budget>2000000)
+        cout << "you can buy a scorpio"<< endl;
+
+    else
+            cout<<"you cant buy a scorpio "<< endl;
+        
+    
+
+    return 0;
+}
