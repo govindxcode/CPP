0f84873dbfa8c7c0fee1698a35dca33bd9c7d7e1
Govind <ranagovind6969@gmail.com>
2026-06-25 20:46:30 +0530

    added if else and if - else if

--- control flow .cpp
+++ control flow .cpp
@@ -1,4 +1,9 @@
-#include<iostream>
+
+
+//              IF-ELSE CONDITION 
+
+
+/*#include<iostream>
 using namespace std;
 
 int main(){
@@ -8,13 +13,46 @@ int main(){
 
     cin >> budget;
 
-    if(budget>2000000)
+    if(budget>2000000){
         cout << "you can buy a scorpio"<< endl;
-
+    }
     else
-            cout<<"you cant buy a scorpio "<< endl;
-        
-    
+           { cout<<"you cant buy a scorpio "<< endl;
 
+           }
+    return 0;
+}*/
+
+
+
+
+//           IF -  ELSE-IF  CONDITION
+
+
+#include<iostream>
+using namespace std ;
+
+int main (){
+
+    int marks;
+    cout<< "enter your marks "<< endl;
+
+    cin>> marks;
+
+    if ( marks>=81 ){
+        cout<<" Grade A" << endl;
+    }
+
+    else if ( marks<=80 && marks>=75 ){
+        cout<<" Grade B" << endl;
+    }
+
+    else if ( marks<74 && marks>= 60){
+        cout<<" Grade C" << endl;
+    }
+
+    else if ( marks<60 && marks>=30){
+        cout<<" Grade D" << endl;
+    }    
     return 0;
 }
